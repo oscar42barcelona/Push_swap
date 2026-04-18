37e827ef96a30c6276ea4daf1f9bc6626d8d0a86
oscar_42 <suarezgonzalezoscar36@gmail.com>
2026-04-18 13:33:54 +0200

    operaciones swap y push

--- operaciones_oscar.c
+++ operaciones_oscar.c
@@ -6,7 +6,7 @@
 /*   By: osuarez- <osuarez-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 14:26:04 by osuarez-          #+#    #+#             */
-/*   Updated: 2026/04/17 17:20:31 by osuarez-         ###   ########.fr       */
+/*   Updated: 2026/04/18 13:33:06 by osuarez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
  
 void	sa(t_stack **a)
 {
-	
-	t_stack	*first;
-	t_stack	*second;
-	
+	t_stack *first;
+	t_stack *second;
 
 	if (!a)
 		return;
@@ -25,8 +23,61 @@ void	sa(t_stack **a)
 		return;
 	first = *a;
 	second = (*a)->next;
-
-	first = (*a)->next->next;
+	first->next = second->next;
 	second->next = first;
 	*a = second;
 }
+
+void    sb(t_stack **b)
+{
+    t_stack *first;
+    t_stack *second;
+
+    if (!b)
+        return;
+    if (ft_lstsize(*b) < 2)
+        return; //o gestionarlo como error...
+    first = *b;
+    second = (*b)->next;
+    first->next = second->next;
+    second->next = first;
+    *b = second;
+}
+
+void	ss(t_stack **a, t_stack **b)
+{
+	sb(b);
+	sa(a);
+}
+
+void	pa(t_stack **a, t_stack **b)
+{
+	t_stack	*first_b;
+
+	if (!b || !*b)
+		return; //o gestionarlo como error...
+	first_b = *b;
+	*b = (*b)->next;
+	first_b->next = *a;
+	*a = first_b;
+}
+
+
+void	pb(t_stack **a, t_stack **b)
+{
+	t_stack	*first_a;
+
+	if (!a || !*a)
+		return; //o gestionarlo como error...
+	first_a = *a;
+	*a = (*a)->next;
+	first_a->next = *b;
+	*b = first_a;
+}
+
+void    ss(t_stack **a, t_stack **b)
+{
+    sb(b);
+    sa(a);
+}
+
